0230e9e3f552d59e30f1ec804eea423183e07e15
Boodhicode <152362424+Boodhicode@users.noreply.github.com>
2024-07-12 18:02:27 +0700

    .

--- src/Layout/ProductsNotInCart.jsx
+++ src/Layout/ProductsNotInCart.jsx
@@ -4,10 +4,10 @@ const ProductsNotInCart = () => {
   const navigate = useNavigate();
 
   return (
-    <div role="main">
+    <div role="main" className='container-shopify not-cart'>
       <div>
         <div></div>
-        <div>
+        <div className='goto'>
           <h1>Your shopping cart is empty</h1>
           <p>Lets fill it with your dream items</p>
           <button onClick={() => navigate('/')} aria-label="Go back to Homepage">Go back to Homepage</button>
